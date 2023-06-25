535d3b9ecd6708177e49eca63a088c20cf094f0f
zhumingming <zhu.m.m@outlook.com>
2023-06-25 22:52:35 +0800

    315, but not ac

--- leetcode/315-smaller-count-after-self.cpp
+++ leetcode/315-smaller-count-after-self.cpp
@@ -1,17 +1,28 @@
 #include "common.hpp"
 
 // https://leetcode.cn/problems/count-of-smaller-numbers-after-self/
+// 未通过，不看了
 class Solution {
 public:
     int * temp;
-    void countSmaller(vector<int>& nums) {
+    int * count;
+    std::map<int, int> index_map;
+    vector<int> countSmaller(vector<int>& nums) {
       temp = new int[nums.size()];
+      count = new int[nums.size()];
+      for (int i = 0; i < nums.size(); ++i) {
+        index_map.insert({nums[i], i});
+        count[i] = 0; // 记得要初始化！
+      }
       sort(nums, 0, nums.size() - 1);
+      std::vector<int> res(count, count + nums.size());
       delete []temp;
+      delete []count;
+      return res;
     }
     
     void sort(vector<int>& nums, int left, int right) {
-      if (left >= right) {
+      if (left == right) {
         return;
       }
 
@@ -21,24 +32,27 @@ public:
       sort(nums, left, mid);
       sort(nums, mid + 1, right);
       merge(nums, left, mid, right);
-      for (auto n : nums) {
-        std::cout << n << " ";
-      }
-      std::cout << std::endl;
     }
 
     void merge(vector<int>& nums, int left, int mid, int right) {
-      for (int i = 0; i < nums.size(); ++i) {
+      for (int i = left; i <= right; ++i) {
         temp[i] = nums[i];
       }
       int i = left, j = mid + 1;
-      for (int k = 0; k < nums.size(); ++k) {
+      for (int k = left; k <= right; ++k) {
         if (i == mid + 1) {
           nums[k] = temp[j++];
         } else if (j == right + 1) {
           nums[k] = temp[i++];
-        } else if (nums[i] < nums[j]) {
+          // 这个判断解决-1,-1这个用例
+          if (nums[k] != temp[right]) {
+            count[index_map[nums[k]]] += j - (mid + 1);
+          }
+        } else if (temp[i] < temp[j]) {
           nums[k] = temp[i++];
+          if (nums[k] != temp[j]) {
+            count[index_map[nums[k]]] += j - (mid + 1);
+          }
         } else {
           nums[k] = temp[j++];
         }
@@ -48,14 +62,14 @@ public:
 
 int main(int argc, char **argv)
 {
-  std::vector<int> nums{5, 3, 2, 1, 4, 7, 6, 8};
+  std::vector<int> nums{26,78,27,100,33,67,90,23,66,5,38,7,35,23,52,22,83,51,98,69,81,32,78,28,94,13,2,97,3,76,99,51,9,21,84,66,65,36,100,41};
   for (auto n : nums) {
     std::cout << n << " ";
   }
   std::cout << std::endl;
   Solution s;
-  s.countSmaller(nums);
-  for (auto n : nums) {
+  auto res = s.countSmaller(nums);
+  for (auto n : res) {
     std::cout << n << " ";
   }
   std::cout << std::endl;
